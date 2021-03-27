53b5de308aa8b509431bec4a41c885effdd44bf2
Juan Quispe <jquispeh63@gmail.com>
2021-03-26 19:13:52 -0500

    Adds basic structure to Profile page
    -Modifies location of getData for reloading when error happens
    -Adds basic div structure to show each property of profile info

--- src/Directories/Profile.js
+++ src/Directories/Profile.js
@@ -2,31 +2,53 @@ import { useState, useEffect } from 'react';
 import { Switch, Route, useRouteMatch, useParams } from 'react-router-dom';
 import { getUserData } from './Components/FireHandler';
 
+async function getData(userId) {
+    try {
+        const data_ = await getUserData(userId);
+        return data_;
+    } catch {
+        return;
+    }
+}
+
 const RealProfile = () => {
     const { userId } = useParams();
     const [ data, setData ] = useState({});
 
     useEffect(() => {
-        const getData = async () => {
-            try {
-                const data_ = await getUserData(userId);
-                setData(data_);
-            } catch {
-                setData(null);
-            }
+        async function getId() {
+            setData(await getData(userId));
         }
-        getData();
+        getId();
     }, [userId]);
 
     return (
     <>
-        {data ?
-        (data.username ?
-        JSON.stringify(data)
-        :
-        <p>Loading...</p>)
-        :
-        <p>Error while loading info</p>}
+        <div className="profile-info">
+            {data ?
+            (data.username ?
+            <>
+                <h1>{data.username}</h1>
+                <div className="description">
+                    {data.description}
+                </div>
+                {Object.keys(data.networking).length > 0 && 
+                <div className="networking">
+                    <h3>Networks:</h3>
+                    {Object.keys(data.networking).map((net, i) => {
+                        const url = data.networking[net];
+                        return <p key={i}>{net}: {url}</p>
+                    })}
+                </div>}
+                <div className="following">
+                    Following: {data.following.length}
+                </div>
+            </>
+            :
+            <p>Loading...</p>)
+            :
+            <p onClick={async () => setData(await getData(userId))}>Error while loading info, click here to try again.</p>}
+        </div>
     </>);
 }
 
@@ -34,16 +56,14 @@ const Profile = () => {
     const match = useRouteMatch();
 
     return (
-    <>
-        <Switch>
-            <Route path={`${match.path}/:userId`}>
-                <RealProfile />
-            </Route>
-            <Route path={match.path}>
-                <p>Introduce a User ID, like "{`${match.path}/{User ID}`}"</p>
-            </Route>
-        </Switch>
-    </>);
+    <Switch>
+        <Route path={`${match.path}/:userId`}>
+            <RealProfile />
+        </Route>
+        <Route path={match.path}>
+            <p>Introduce a User ID, like "{`${match.path}/{User ID}`}"</p>
+        </Route>
+    </Switch>);
 }
 
 export default Profile;
